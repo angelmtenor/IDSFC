c2aa0958778a3eeaf8179a1bd6c267bb91c99bf6
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-05-09 20:36:15 +0200

    feat: gradient descent

---
+++ L3_Data_Analysis/B_gradient_descent.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+
+def compute_cost(features, values, theta):
+    """
+    Compute the cost of a list of parameters, theta, given a list of features 
+    (input data points) and values (output data points).
+    """
+    m = len(values)
+    sum_of_square_errors = np.square(np.dot(features, theta) - values).sum()
+    cost = sum_of_square_errors / (2 * m)
+
+    return cost
+
+
+def gradient_descent(features, values, theta, alpha, num_iterations):
+    """
+    Perform gradient descent given a data set with an arbitrary number of features.
+    """
+
+    # Write code here that performs num_iterations updates to the elements of theta.
+    # times. Every time you compute the cost for a given list of thetas, append it
+    # to cost_history.
+    # See the Instructor notes for hints.
+
+    cost_history = []
+
+    # your code
+    for i in range(num_iterations):
+        predicted = np.dot(features, theta)
+        theta += alpha/len(values) * np.dot((values - predicted), features)
+        cost_history.append(compute_cost(features, values, theta))
+
+    return theta, pd.Series(cost_history)  # leave this line for the grader
